9b9093b187f323f3d7c9fb879047b7a443bd6a50
Drew Moore <cantsnuffthedrewster@gmail.com>
2017-01-02 19:26:06 -0600

    Panics complete.

--- about_panics.go
+++ about_panics.go
@@ -1,14 +1,22 @@
 package go_koans
 
+import "fmt"
+
 func divideFourBy(i int) int {
+  defer func() {
+    if e := recover(); e != nil {
+      fmt.Println("Sorry, yo, can't divide by", e)
+    }
+  }()
+  if i == 0 { panic(i) }
   return 4 / i
 }
 
 const __divisor__ = 0
 
 func aboutPanics() {
-  assert(__delete_me__) // panics are exceptional errors at runtime
+  assert(divideFourBy(__divisor__) == 0) // panics are exceptional errors at runtime
 
-  n := divideFourBy(__divisor__)
+  n := divideFourBy(2)
   assert(n == 2) // panics are exceptional errors at runtime
 }
